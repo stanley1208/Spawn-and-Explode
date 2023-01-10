d662ea62a7bd931bf15466391b1bae81801ff81a
A8250092 <stanley.mei08@gmail.com>
2023-01-10 16:25:22 +0800

    Update MouseButtonProcessor.cs

--- Spawn-and-Explode/Assets/Scripts/MouseButtonProcessor.cs
+++ Spawn-and-Explode/Assets/Scripts/MouseButtonProcessor.cs
@@ -15,14 +15,24 @@ public class MouseButtonProcessor : MonoBehaviour
     // first frame input support
     bool spawnInputOnPreviousFrame = false;
 	bool explodeInputOnPreviousFrame = false;
-
-	/// <summary>
-	/// Update is called once per frame
-	/// </summary>
-	void Update()
+    // need for location of the new character
+    GameObject currentCharacter;
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
 	{
         // spawn teddy bear as appropriate
-
+        if (Input.GetAxis("SpawnTeddyBear") > 0){
+            if (!spawnInputOnPreviousFrame)
+            {
+                spawnInputOnPreviousFrame = true;
+                Vector3 position = currentCharacter.transform.position;
+                currentCharacter = Instantiate<GameObject>(
+                       prefabTeddyBear, position,
+                       Quaternion.identity);
+            }
+        }
         // explode teddy bear as appropriate
 		
 	}
